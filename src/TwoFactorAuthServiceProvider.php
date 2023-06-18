75f51c2866186bef740155d4a68c9fa6d9f8c9c3
hridoyraisul <raisulhridoy@hotmail.com>
2023-06-18 17:31:49 +0600

    updated config

--- src/TwoFactorAuthServiceProvider.php
+++ src/TwoFactorAuthServiceProvider.php
@@ -8,16 +8,19 @@ class TwoFactorAuthServiceProvider extends \Illuminate\Support\ServiceProvider
     {
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-
+        $this->mergeConfigFrom(
+            __DIR__.'/config/laravel2fa.php', 'laravel2fa'
+        );
         $this->publishes([
             __DIR__.'/config/laravel2fa.php' => config_path('laravel2fa.php'),
+        ]);
+        $this->publishes([
+            __DIR__.'/database/migrations' => database_path('migrations'),
         ]);
     }
 
     public function register()
     {
-        $this->mergeConfigFrom(
-            __DIR__.'/config/laravel2fa.php', 'laravel2fa'
-        );
+        //
     }
 }
